4ca0e2f316f056df87c48a880675d22b0bcb2611
Alexander Litovkin <litovkinalexan@gmail.com>
2026-01-15 18:50:15 +0700

    init commit

---
+++ src/main/java/org/allitov/wiremocksandbox/WiremockSandboxApplication.java
@@ -0,0 +1,13 @@
+package org.allitov.wiremocksandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockSandboxApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(WiremockSandboxApplication.class, args);
+    }
+
+}
